c4efe5d02c39a735b84d17398303f392fc1cd7d6
Mohamedrazzak1 <134249790+Mohamedrazzak1@users.noreply.github.com>
2024-02-04 13:20:43 +0530

    Update variables.tf

--- eks_module/variables.tf
+++ eks_module/variables.tf
@@ -1,21 +1,14 @@
 
 variable "access_key" {
-    default = ""
 }
 variable "secret_key" {
-    default = ""
 }
-
-
 variable "region" {
-    default = "us-east-1"
 }
 
-variable "aws_eks_cluster_config" {
-  
+variable "aws_eks_cluster_config" { 
 }
-variable "eks_node_group_config" {
-  
+variable "eks_node_group_config" { 
 }
 
 
